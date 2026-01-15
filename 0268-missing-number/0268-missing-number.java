a6c12f58892bc56c4e020febbb4c13c318181dc9
sunilmr06 <smr662003@gmail.com>
2026-01-15 17:52:43 +0530

    Time: 0 ms (100%), Space: 47.4 MB (54.37%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int allXOR = 0;
+
+        for(int i=0; i<=nums.length;i++)
+        {
+            allXOR = allXOR ^ i;
+        }
+
+        for (int num : nums)
+        {
+            allXOR = allXOR ^ num;
+        }
+        return allXOR;
+    }
+}
