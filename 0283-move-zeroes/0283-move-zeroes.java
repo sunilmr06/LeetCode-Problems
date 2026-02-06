d34fbed888729cab55ed92f39ac256c8f82e74b4
sunilmr06 <smr662003@gmail.com>
2026-02-06 21:20:30 +0530

    Time: 2 ms (90.03%), Space: 47.6 MB (85.69%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,7 +2,7 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int k =0;
 
-        for(int i=0; i<nums.length;i++)
+        for(int i=0; i<nums.length; i++)
         {
             if(nums[i] != 0)
             {
@@ -16,6 +16,5 @@ class Solution {
             nums[k] = 0;
             k++;
         }
-        
     }
 }
