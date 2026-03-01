9fefbe1067c1deb6fd6c8d0a469c6d168ce346a6
sunilmr06 <smr662003@gmail.com>
2026-03-01 08:06:17 +0530

    Time: 5 ms (81.61%), Space: 77.5 MB (27.72%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length -1;
+        int maxarea =0;
+        while(left <right)
+        {
+            int width = right - left;
+            int hight = Math.min(height[left], height[right]);
+            int area = width * hight;
+
+            maxarea = Math.max(maxarea, area);
+
+            if(height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return maxarea;
+    }
+}
