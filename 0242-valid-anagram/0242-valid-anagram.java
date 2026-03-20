e1be51d445bf7999c6f389502bc6956e60a1369a
Sunil M R <smr662003@gmail.com>
2026-03-20 08:28:44 +0530

    Time: 3 ms (92.3%), Space: 46.4 MB (44.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,19 +4,12 @@ class Solution {
         {
             return false;
         }
-        int[] countChar = new int[26];
-        for(int i=0; i<s.length(); i++)
-        {
-            countChar[s.charAt(i) - 'a']++;
-            countChar[t.charAt(i) - 'a']--;
-        }
-        for(int count : countChar)
-        {
-            if(count != 0)
-            {
-                return false;
-            }
-        }
-        return true;
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a,b);
     }
 }
