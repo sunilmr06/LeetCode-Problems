9e49881e18cb83eea54f3ea5a38f846b5a50c5d5
Sunil M R <smr662003@gmail.com>
2026-03-30 07:38:27 +0530

    Time: 0 ms (100%), Space: 42.9 MB (78.15%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -3,7 +3,7 @@ class Solution {
         int length = 0;
         int index = s.length() -1;
 
-        while(index >=0 && s.charAt(index) == ' ')
+        while(index >= 0 && s.charAt(index) == ' ')
         {
             index--;
         }
