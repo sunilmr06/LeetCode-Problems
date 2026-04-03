0a82bdd14670f6efef45ef1f4d66a4d183806e30
Sunil M R <smr662003@gmail.com>
2026-04-03 12:32:27 +0530

    Time: 0 ms (100%), Space: 42.7 MB (97.12%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,53 +1,5 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-
-        int[] lps = buildLPS(needle);
-
-        int i = 0, j = 0;
-
-        while(i < haystack.length()) {
-
-            if(haystack.charAt(i) == needle.charAt(j)) {
-                i++;
-                j++;
-            }
-
-            if(j == needle.length()) {
-                return i - j;
-            } 
-            else if(i < haystack.length() && 
-                    haystack.charAt(i) != needle.charAt(j)) {
-
-                if(j != 0) {
-                    j = lps[j - 1];
-                } else {
-                    i++;
-                }
-            }
-        }
-
-        return -1;
-    }
-
-    private int[] buildLPS(String pattern) {
-        int[] lps = new int[pattern.length()];
-        int len = 0, i = 1;
-
-        while(i < pattern.length()) {
-            if(pattern.charAt(i) == pattern.charAt(len)) {
-                len++;
-                lps[i] = len;
-                i++;
-            } else {
-                if(len != 0) {
-                    len = lps[len - 1];
-                } else {
-                    lps[i] = 0;
-                    i++;
-                }
-            }
-        }
-
-        return lps;
+         return haystack.indexOf(needle);
     }
 }
