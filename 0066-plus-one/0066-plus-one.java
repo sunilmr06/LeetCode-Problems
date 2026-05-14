d84dbe154ab01958e7c3b760f3b9756896567717
Sunil M R <smr662003@gmail.com>
2026-05-14 08:07:44 +0530

    Time: 0 ms (100%), Space: 43.3 MB (77.59%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,17 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i=digits.length-1; i>=0; i--)
+        for(int i=digits.length-1; i>=0;i--)
         {
             if(digits[i] < 9)
             {
                 digits[i]++;
                 return digits;
             }
-            digits[i] = 0; 
+            digits[i]=0;
         }
-        int[] result = new int[digits.length + 1];
-            result[0] = 1;
-
-            return result; 
+        int[] result = new int[digits.length+1];
+        result[0]=1;
+        return result;
     }
 }
