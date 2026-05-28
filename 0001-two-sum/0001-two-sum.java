e02938559a83f8311579fb66e5934ca13344a4d2
Sunil M R <smr662003@gmail.com>
2026-05-28 21:01:03 +0530

    Begin implementation of twoSum method

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-
         for(int i=0; i<nums.length; i++)
         {
             int component = target - nums[i];
