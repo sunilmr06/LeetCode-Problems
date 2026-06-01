d6b5b4ee426b2a0a762bddce489804c69205ab11
Sunil M R <smr662003@gmail.com>
2026-06-01 08:03:59 +0530

    Time: 2 ms (99.38%), Space: 47.3 MB (23.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,13 +3,13 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<nums.length; i++)
         {
-            int component = target - nums[i];
-            if(map.containsKey(component))
+            int componet = target - nums[i];
+            if(map.containsKey(componet))
             {
-                return new int[] {map.get(component), i};
+                return new int[] {map.get(componet), i};
             }
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
-        return new int[]{};
+        return new int[] {};
     }
 }
