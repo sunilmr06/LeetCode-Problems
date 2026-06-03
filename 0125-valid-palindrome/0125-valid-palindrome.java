992129e9b51c78a86490d34d605892b4e0b9735a
Sunil M R <smr662003@gmail.com>
2026-06-03 08:21:22 +0530

    Time: 2 ms (99.17%), Space: 44 MB (90.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
         int left =0;
-        int right = s.length()-1;
+        int right = s.length() -1;
 
         while(left < right)
         {
