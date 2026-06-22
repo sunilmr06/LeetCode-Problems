74b8e9fcd1efc0557936f41d7a58c30de7413fd1
Sunil M R <smr662003@gmail.com>
2026-06-22 08:05:12 +0530

    Time: 2 ms (99.21%), Space: 44.3 MB (75.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
         int left = 0;
-        int right = s.length() -1;
+        int right = s.length()-1;
 
         while(left < right){
             char l = s.charAt(left);
@@ -15,6 +15,7 @@ class Solution {
                 right--;
                 continue;
             }
+
             if(Character.toLowerCase(l) != Character.toLowerCase(r)){
                 return false;
             }
