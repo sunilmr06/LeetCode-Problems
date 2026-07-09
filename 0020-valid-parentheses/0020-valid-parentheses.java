c248667a074bc3c897e4ddc3c1a882330cc848e7
Sunil M R <smr662003@gmail.com>
2026-07-09 08:21:25 +0530

    Time: 3 ms (87.15%), Space: 43.2 MB (57.85%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,25 +4,24 @@ class Solution {
 
         for(char ch : s.toCharArray())
         {
-            if(ch == '{' || ch == '(' || ch == '[')
-            {
+            if(ch == '(' ||
+            ch == '{' ||
+            ch == '['){
                 stack.push(ch);
             }
             else
             {
-                if(stack.isEmpty())
-                {
+                if(stack.isEmpty()){
                     return false;
                 }
 
                 char top = stack.pop();
 
-                if((ch == '}' && top != '{') ||
-                    (ch == ')' && top != '(') ||
-                    (ch == ']' && top != '['))
-                    {
-                        return false;
-                    }
+                if(ch == ')' && top != '(' 
+            ||   ch == '}' && top != '{'
+            ||  ch == ']' && top != '['){
+                return false;
+            }
             }
         }
         return stack.isEmpty();
